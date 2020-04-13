a3fed39d8eac18adc28b196a2e7a157d8bffcaaf
eunery <asdad@yaofj.com>
2020-04-13 21:56:59 +0500

    GameLab 1-2lvl
    3lvl in progress...

---
+++ MiniGame/MiniGame/Level2.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniGame
+{
+    public partial class Level2 : Window
+    {
+        private int answerFlag = 1;
+        public Level2()
+        {
+            InitializeComponent();
+            AnswerBox.TextChanged += AnswerBox_TextChanged;
+        }
+
+        private void AnswerBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            switch (answerFlag)
+            {
+                case 1:
+                    AskLabel.Content = "Корень из 225?";
+                    if (AnswerBox.Text == "15")
+                    {
+                        answerFlag = 2;
+                        AnswerBox.Text = "";
+                    }
+                    break;
+                case 2:
+                    AskLabel.Content = "Сколько ног у сороконожки?";
+                    if (AnswerBox.Text == "много")
+                    {
+                        answerFlag = 3;
+                        AnswerBox.Text = "";
+                    }
+                    break;
+                case 3:
+                    AskLabel.Content = "Сколько лет длилась столетняя война?";
+                    if (AnswerBox.Text == "116")
+                    {
+                        Level3 lvl3 = new Level3();
+                        lvl3.Show();
+                        LevelTwo.Close();
+                        AnswerBox.Text = "";
+                    }
+                    break;
+            }
+        }
+    }
+}
--- MiniGame/MiniGame/MainWindow.xaml.cs
+++ MiniGame/MiniGame/MainWindow.xaml.cs
@@ -12,17 +12,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MiniGame
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rnd = new Random();
+        private int tickCounter = 10;
+        private DispatcherTimer timer;
+        private int rndNumber = 10;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Tick += timertick;
+            timer.Start();
+            Button.Click += Button_Click;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rndNumber = rnd.Next(10,99);
+            string sym = rndNumber.ToString();
+            if (sym[0] == sym[1])
+            {
+                timer.Stop();
+                Level2 lvl2 = new Level2();
+                lvl2.Show();
+                Level1.Close();
+            }
+        }
+        private void timertick(object sender, EventArgs e)
+        {
+            if (tickCounter > 0)
+            {
+                tickCounter--;
+                TimerBlockDown.Text = String.Format("00:0{0}:{1}", tickCounter/60, tickCounter%60);
+            }
+            else
+            {
+                timer.Stop();
+                MessageBox.Show("Гуляй братишка");
+            }
         }
     }
 }
